c193c0f8967d6594e0a8803c0b660cbdeb80f807
AVVKavvk <kumawatvipin066@gmail.com>
2023-06-14 16:36:22 +0530

    5lab

--- src/Labs/Sem6.js
+++ src/Labs/Sem6.js
@@ -1,9 +1,50 @@
 import React from 'react'
-
+import ll from "../image/cc.png";
+import lll from "../image/cg.png";
 function Sem6() {
   return (
-    <div class="flex mx-auto  justify-center items-center h-[60vh]">
-        <h1 class="text-2xl p-9 rounded-lg  bg-red-300"> Currently Unavaiable </h1></div>
+    <div class=" sm:w-[500px]  md:max-w-[1000px] grid grid-cols-1  sm:grid-cols-2 mx-auto relative gap-2 pt-4 space-y-5 mt-8 overflow-hidden">
+      <div class="relative flex flex-col justify-center items-center text-center  w-full h-[250px] space-x-16  px-5 ">
+        <div>
+          <a
+            href="https://drive.google.com/drive/folders/13dBBnCwWxlbvxcH1uG0JW48mF06M8o01?usp=sharing"
+            target="_blank"
+          >
+            <div>
+              <img
+                src={ll}
+                alt=""
+                class="hover:scale-110 transition-all duration-500"
+                height="170px"
+                width="170px"
+              />
+            </div>
+          </a>
+
+          <h1 class=" z-10 top-[60px] left-[80px] text-4xl mt-3 ">CC</h1>
+        </div>
+      </div>
+      <div class="relative flex flex-col justify-center items-center text-center  w-full h-[250px] space-x-16  px-5 ">
+        <div>
+          <a
+            href="https://drive.google.com/drive/folders/1-oI5uHzX53KvROLAO9J845au9nsnYo6b?usp=sharing"
+            target="_blank"
+          >
+            <div>
+              <img
+                src={lll}
+                alt=""
+                class="hover:scale-110 transition-all duration-500"
+                height="170px"
+                width="170px"
+              />
+            </div>
+          </a>
+
+          <h1 class=" z-10 top-[60px] left-[80px] text-4xl mt-3 ">CG</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
