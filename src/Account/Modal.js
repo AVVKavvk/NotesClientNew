6fe3068ed2c22ae305ec2b5b167c98d8208d9c86
AVVKavvk <kumawatvipin066@gmail.com>
2025-01-01 18:40:25 +0530

    revert change

--- src/Account/Modal.js
+++ src/Account/Modal.js
@@ -4,7 +4,7 @@ function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
 
   return (
-    <div className=" fixed  inset-8 flex  z-50 bg-black bg-opacity-50">
+    <div className=" fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white w-[90%] max-w-md rounded-lg p-6 shadow-lg relative">
         <button
           onClick={onClose}
